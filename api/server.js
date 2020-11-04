75e48c38813ca4b9a82c22aeef98b02485b21951
Chaya Otikor <c.otikor@gmail.com>
2020-11-04 11:50:13 -0700

    added error handling middleware

--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const configureMiddleware = require("../middleware/globalMiddleware");
+const errorHandler = require("../middleware/errorMiddleware");
+const appointmentRoutes = require("../routes/appointmentRoutes");
+const noteRoutes = require("../routes/noteRoutes");
+const studentRoutes = require("../routes/studentRoutes");
 
 const server = express();
 
 configureMiddleware(server);
 
-const appointmentRoutes = require("../routes/appointmentRoutes");
-const noteRoutes = require("../routes/noteRoutes");
-const studentRoutes = require("../routes/studentRoutes");
-
-
 server.use('/appointments', appointmentRoutes)
 server.use('/notes', noteRoutes)
 server.use('/students', studentRoutes)
 
+server.use(errorHandler);
 
 module.exports = server;
---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,43 @@
+const responseStatus = require("../config/responseStatusConfig");
+
+function errorHandler(error, req, res, next) {
+  switch (error) {
+    case responseStatus.badRequest:
+      res
+        .status(responseStatus.badRequest)
+        .json({
+          statusCode: error,
+          message: "Required fields cannot be blank.",
+        });
+      break;
+    case responseStatus.notFound:
+      res
+        .status(responseStatus.notFound)
+        .json({ statusCode: error, message: "This resource does not exist." });
+      break;
+    case responseStatus.serverError:
+      res.status(responseStatus.serverError).json({
+        statusCode: error,
+        message: `The request could not be completed. Please try again.`,
+      });
+
+      break;
+    case responseStatus.badCredentials:
+      res
+        .status(responseStatus.badCredentials)
+        .json({
+          statusCode: error,
+          message: "Incorrect credentials. Please try again.",
+        });
+    case responseStatus.forbiddenAccess:
+      res.status(responseStatus.forbiddenAccess).json({
+        statusCode: error,
+        message: "You are not authorized to view this content.",
+      });
+    default:
+      res.json({ status: error, message: error.message });
+  }
+  next();
+}
+
+module.exports = errorHandler;
